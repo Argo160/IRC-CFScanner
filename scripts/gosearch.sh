27f8d0bc8e7831ce318c39a522191ed94bd7dd1c
Argo160 <116899376+Argo160@users.noreply.github.com>
2023-02-13 14:39:49 +0330

    Delete gosearch.sh

--- scripts/gosearch.sh
+++
@@ -1,8 +0,0 @@
-clear
-cd Solo-CFScanner/scripts
-chmod +x v2ctl v2ctl-mac v2ray v2ray-mac
-sleep 2
-clear
-curl -s http://bot.sudoer.net/config.real -o ./config.real
-sleep 10
-bash cfFindIP.sh ./config.real
